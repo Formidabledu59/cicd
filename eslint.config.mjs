a0bc3031e8a1e895d385910f85c34afd2e8bd73e
Ayoub Fathallah <130578160+Formidabledu59@users.noreply.github.com>
2024-11-16 14:17:56 +0100

    jobs lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,22 @@
+import * as globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: ["**/*.test.js"],
     files: ["**/*.js"],
-    languageOptions: { sourceType: "commonjs" },
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.node, // Correction ici
+    },
   },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
       "no-console": "error",
-      "constructor-super": "off",  // Désactiver
     },
   },
 ];
